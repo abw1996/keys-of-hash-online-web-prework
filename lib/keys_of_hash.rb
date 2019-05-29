8ce63dd3e766a1cd3b27fc7a1418689ea90599d1
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-29 18:16:03 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,5 @@
 class Hash
   def keys_of(arguments)
      returning_array = []
-    Hash.each do |key, value|
-      if arguments.include? value          
-        returning_array.push(key)
-      end 
-    end 
-  end
+      binding.pry
 end

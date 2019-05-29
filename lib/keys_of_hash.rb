34f3db878786d2d93694c6582f47b6dbc8074f4c
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-29 18:44:57 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    returning_array = []
+    self.each do |key, value|
+      if arguments.include?(value)
+        returning_array.push(key)
+      end
+    end
+    returning_array
   end
 end
+
